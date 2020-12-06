df5f34e651db0c5b7d5eb3c820454ac02335fb6a
Ilja <iljakristafenko@gmail.com>
2020-12-06 21:06:58 +0200

    new lesson JS

---
+++ starter/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriversLicense = true;
+//              //!  If passTest = true -->> doing hasDriv = true and if hasDriv = true -->console.log .....
+// if(hasDriversLicense) {
+//     console.log("I can drive :) ");
+// }
+
+// console.log(hasDriversLicense);
+
+
+// function fruitMashin(apple, orange) {
+//     console.log(apple,orange);// 5 --- 8 
+//     const result = (`${apple} apples + ${orange} orange = very good Juice ! `);
+//     // console.log(result);
+//     return result;                     //! in to function
+// }
+
+// fruitMashin(10, 8);          //!   RESUL PUT IN TO FUNCTION --> fruitMashin
+// console.log(fruitMashin(15,8));
+// const resultMashine = fruitMashin(5, 8);
+// console.log(resultMashine);
+
+
+// function describeCountry(county, population, capitalCity) {
+//    return (`${county} has ${population} million people and capital city is ${capitalCity}`);
+// }
+
+
+// console.log(describeCountry("Finland", 6, "Helsinki"));  // без сохран в переменную
+
+// const taskB = describeCountry("Latvija", 2, "Riga");
+// const taskC = describeCountry("Lietuva", 3, "Vilnus");   // with SAFE variables 
+
+// console.log(taskB);
+// console.log(taskC);
+
+
+// function describeCountry(country, population, capitalCity) {
+//     return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//     }
+
+// const descPortugal = describeCountry('Portugal', 10,
+// 'Lisbon');
+// const descGermany = describeCountry('Germany', 83,
+// 'Berlin');
+// const descFinland = describeCountry('Finland', 6,
+// 'Helsinki');
+// console.log(descPortugal);
+// console.log(descGermany);
+// console.log(descFinland);
+
+
