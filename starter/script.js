bd1da669e470b591c83f1df49b70adce7ad21b6e
Ilja <iljakristafenko@gmail.com>
2020-12-06 22:20:43 +0200

    arrow function expression and declaration

--- starter/script.js
+++ starter/script.js
@@ -54,3 +54,53 @@
 // console.log(descFinland);
 
 
+
+  // Arrow functions          //! Так что это отлично подходит для простых однострочных функций.
+
+//   const calcAge3 = birthYeah => 2037 - birthYeah;
+//   const age3 = calcAge3(1991);
+//   console.log(age3);
+
+//   const yearsUntilRetirement = (birthYeah, firstName) => {
+//     const age = 2037 - birthYeah;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years`;
+//   };
+
+//   const calcAge33 = () => console.log("hi"); //!  Arrow function without arguments.  
+//   calcAge33(); //! call function
+
+function percentageOfWorld1 (population) {
+    const peopleWorld = 7900;
+     const curentReturnPopul = population / peopleWorld * 100;    //todo : Prercentage of the 7900. 
+     return curentReturnPopul;
+     //! РОВНО >>>>>>>>  return (population / 7900) * 100;
+}
+
+const procentPopulation = percentageOfWorld1(1441);
+console.log(Math.round(procentPopulation));
+
+const procentPopulation1 = percentageOfWorld1(800);
+console.log(Math.round(procentPopulation1));
+
+const procentPopulation2 = percentageOfWorld1(141);
+console.log(Math.round(procentPopulation2));
+
+
+
+const percentOfWorld2 = function (population) { 
+    return (population / 7900) * 100;
+};
+
+console.log(percentOfWorld2(1441));
+console.log(percentOfWorld2(800));
+const usa = percentOfWorld2(141);
+console.log(usa);
+
+
+const percentageOfWorld3 = (population) => population / 7900 * 100;
+
+const percPortugal3 = percentageOfWorld3(10);
+console.log(percentageOfWorld3(2000));
+console.log(percPortugal3);
